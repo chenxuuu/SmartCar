95fe7b1d565b03214eae85528b2e173ce04af3a3
chenxuuu <liucx1@qq.com>
2016-06-12 15:40:03 +0800

    一键编译烧写

--- for_test/build.bat
+++ for_test/build.bat
@@ -1,5 +1,6 @@
+start jflashlite
 :build
-IarBuild.exe Prj\IAR\vcan_Kinetis.ewp -make FN15_Debug -log info
 echo 按任意键再次编译
 pause
+IarBuild.exe Prj\IAR\vcan_Kinetis.ewp -make FN15_Debug -log info
 goto build
